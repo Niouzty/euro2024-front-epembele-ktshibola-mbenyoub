968600a14ca7390ae30b3132a549e8374b76a227
Niouzty <epembelefuala@iut.univ-paris8.fr>
2025-04-03 00:23:42 +0200

    Creation composant comparaisonEquipes

--- euro2024/src/app/app.routes.ts
+++ euro2024/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import {TableListComponent} from './features/table-list/table-list.component';
 import { HomeComponent } from './features/home/home.component';
 import {EquipeListComponent} from './features/equipe-list/equipe-list.component';
 import { TopButeursComponent } from './features/top-buteurs/top-buteurs.component'; 
-
+import { ComparaisonEquipesComponent } from './features/comparaison-equipes/comparaison-equipes.component';
 import { HttpClientModule } from '@angular/common/http';
 
 export const routes: Routes = [
     {path: '',component: HomeComponent},
     { path: 'table-list', component: TableListComponent },
     {path:'equipe-list', component: EquipeListComponent},
-    {path:'top-buteurs', component: TopButeursComponent}
+    {path:'top-buteurs', component: TopButeursComponent},
+    {path:'comparaison-equipes', component: ComparaisonEquipesComponent}
 ];
